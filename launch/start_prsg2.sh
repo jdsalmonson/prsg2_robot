e036e76cb0dc689c80939130d3a9c134cd0b1aff
jdsalmonson <jdsalmonson@gmail.com>
2016-11-06 22:12:06 -0800

    A shell script to start the prsg2 ros node. source ./start_prsg2.sh

---
+++ launch/start_prsg2.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+# Author: Jay Salmonson <jdsalmonson@gmail.com>
+#
+# Please remove the "Author" lines above and replace them
+# with your own name if you copy and modify this script.
+
+# PATH should only include /usr/* if it runs after the mountnfs.sh script
+HOSTNAME=`/bin/hostname`
+# PATH=/usr/local/bin:$PATH
+# PYTHONPATH=/usr/local/lib/python2.7/site-packages:/usr/local/lib/python:$PYTHONPATH
+# NAME=roscore
+# DAEMON=/opt/ros/kinetic/bin/$NAME
+# DAEMONUSER=mcp
+# ROSCORE_LOG=/media/LOG/roscore.log
+# ROSCORE_ERR=/media/LOG/roscore.err
+export ROS_MASTER_URI=http://$HOSTNAME.local:11311
+export ROS_IP=`hostname -I | cut -d' ' -f1`
+# PIDDIR=/var/run/roscore
+# PIDFILE=$PIDDIR/$NAME.pid
+#. /opt/ros/kinetic/setup.sh
+. $HOME/catkin_ws/devel/setup.bash
+# date >> $ROSCORE_LOG
+# date >> $ROSCORE_ERR
+# echo $PATH >> $ROSCORE_LOG
+# echo $PYTHONPATH >> $ROSCORE_LOG
+# DESC="ROS core process"
+# DAEMON_ARGS=""
+# SCRIPTNAME=/etc/init.d/$NAME
+
+# last line of /etc/sudoers removes need for password
+sudo modprobe --first-time i2c-dev
+
+roslaunch prsg2_robot prsg2_controller.launch
